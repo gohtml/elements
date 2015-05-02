64d4a461e585fab18ecdf3947a63dec1e247f433
David Deng <daviddengcn@gmail.com>
2015-05-02 01:25:55 -0700

    ADD basic types

---
+++ elements.go
@@ -0,0 +1,10 @@
+package elements
+
+// A string type representing an escaped HTML code
+type HTMLNode string
+
+// A string type representing a URL.
+type URL string
+
+// A string type representing an escaped URL query part.
+type QUERY string
